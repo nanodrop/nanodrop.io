70e07dffefd6ac7e65c19744030ed89d5c8aef68
anarkrypto <anarkrypto@gmail.com>
2023-09-08 00:42:29 -0300

    ✨ Add AddressInput

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+'use client'
+
+import AddressInput from "@/components/AddressInput";
 import Hero from "@/components/Hero";
+import { useState } from "react";
 
 export default function Home() {
+  const [nanoAddress, setNanoAddress] = useState<string | null>(null);
   return (
     <>
       <section className="flex flex-col flex-1 justify-center items-center py-8">
         <Hero />
         <div className="w-96 sm:w-[448px] max-w-full px-4 py-4 sm:py-8">
+          <AddressInput
+            onValidAddress={setNanoAddress}
+            onInvalidAddress={() => setNanoAddress(null)}
+            onSubmit={console.log}
+          />
         </div>
       </section>
     </>
