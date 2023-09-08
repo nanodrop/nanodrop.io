f3805669d8fdf6a95bcc534fcd6780f62d17165f
anarkrypto <anarkrypto@gmail.com>
2023-09-08 00:38:21 -0300

    ✨ Add Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-    </main>
-  )
+    <>
+      <section className="flex flex-col flex-1 justify-center items-center py-8">
+        <Hero />
+        <div className="w-96 sm:w-[448px] max-w-full px-4 py-4 sm:py-8">
+        </div>
+      </section>
+    </>
+  );
 }
