cfe0f2ddad5d3a3de221737a11c68f57923038d0
anarkrypto <anarkrypto@gmail.com>
2023-09-09 07:26:22 -0300

    ✨ Add Appbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
+import Appbar from '@/components/Appbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				<div className="min-h-screen flex flex-col">
+					<Appbar />
 					<main className="flex flex-col flex-1 items-center bg-white border-y border-slate-100">
 						<div className="flex flex-col flex-1 justify-center max-w-7xl">
 							{children}
