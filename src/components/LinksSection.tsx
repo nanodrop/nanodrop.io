8b1c946ef772f2dad77c4ed56ba0da4cf5005b76
anarkrypto <anarkrypto@gmail.com>
2023-09-11 06:59:02 -0300

    📱 Improve LinksSection responsiveness

--- src/components/LinksSection.tsx
+++ src/components/LinksSection.tsx
@@ -3,59 +3,39 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function LinksSection() {
 	return (
-		<section className="grid grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-4 lg:gap-6 py-3 sm:py-6 px-4">
+		<section className="py-2 sm:py-6 px-4 grid grid-cols-1 sm:grid-cols-2 gap-1">
 			<a
 				href="https://hub.nano.org/wallets"
-				className="p-3 border border-slate-200 hover:shadow rounded-md"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<h2
-					className={`${inter.className} text-slate-600 font-medium uppercase`}
-				>
-					Wallets<span>-&gt;</span>
-				</h2>
-				<p
-					className={`${inter.className} text-xs sm:text-sm text-slate-600 font-light`}
-				>
-					Choose your wallet and store your Nano (XNO) securely.
-				</p>
+				<div className="w-full bg-white rounded-md p-3 border border-slate-200 hover:shadow">
+					<h2
+						className={`${inter.className} text-slate-600 font-medium uppercase text-sm sm:text-base`}
+					>
+						Wallets<span>-&gt;</span>
+					</h2>
+					<p
+						className={`${inter.className} text-xs sm:text-sm text-slate-600 font-light`}
+					>
+						Choose your wallet and store your Nano (XNO) securely.
+					</p>
+				</div>
 			</a>
 
-			<a
-				href="/donate"
-				className="p-3 border border-slate-200 hover:shadow rounded-md"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<h2
-					className={`${inter.className} text-slate-600 font-medium uppercase`}
-				>
-					Donate <span>-&gt;</span>
-				</h2>
-				<p
-					className={`${inter.className} text-xs sm:text-sm text-slate-600 font-light`}
-				>
-					Help to mantain NanoDrop.
-				</p>
-			</a>
-
-			<a
-				href="/docs"
-				className="p-3 border border-slate-200 hover:shadow rounded-md"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<h2
-					className={`${inter.className} text-slate-600 font-medium uppercase`}
-				>
-					API <span>-&gt;</span>
-				</h2>
-				<p
-					className={`${inter.className} text-xs sm:text-sm text-slate-600 font-light`}
-				>
-					Hey Devs, check our NanoDrop API.
-				</p>
+			<a href="/donate" target="_blank" rel="noopener noreferrer">
+				<div className="w-full bg-white rounded-md p-3 border border-slate-200 hover:shadow">
+					<h2
+						className={`${inter.className} text-slate-600 font-medium uppercase text-sm sm:text-base`}
+					>
+						Donate<span>-&gt;</span>
+					</h2>
+					<p
+						className={`${inter.className} text-xs sm:text-sm text-slate-600 font-light`}
+					>
+						Help to maintain Nanodrop.
+					</p>
+				</div>
 			</a>
 		</section>
 	)
