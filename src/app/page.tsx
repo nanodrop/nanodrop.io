858477b2431c91c48e24e54b9e0dd45415025556
anarkrypto <anarkrypto@gmail.com>
2023-09-11 07:28:12 -0300

    🎨 Improve price ticker code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,9 @@ export default function Home() {
 	const [nanoAddress, setNanoAddress] = useState<string | null>(null)
 	return (
 		<>
-			<section className="w-full flex flex-col font-light py-6 divide-x divide-slate-200 sm:justify-between uppercase text-slate-500 text-sm sm:text-xl bg-white">
-				<div className="flex justify-center px-4 flex-wrap flex-1 text-center space-x-2">
-					<div className="whitespace-nowrap py-1 px-2 border border-slate-100 bg-slate-50 rounded">
-						1 XNO = 0.67 USD
-					</div>
+			<section className="flex flex-col w-full justify-center items-center py-8 bg-white">
+				<div className="whitespace-nowrap py-1 px-2 border border-slate-100 bg-slate-50 rounded text-slate-500 text-sm sm:text-base">
+					1 XNO = 0.67 USD
 				</div>
 			</section>
 			<section className="flex flex-col w-full flex-1 justify-center items-center py-8 bg-white">
