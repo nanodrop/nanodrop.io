f6511d157dcf7fd1679eb89f2bc7dbb2270a5f03
anarkrypto <anarkrypto@gmail.com>
2023-09-11 07:13:20 -0300

    💬 Add tos message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import AddressInput from '@/components/AddressInput'
 import CheckBox from '@/components/Checkbox'
 import Hero from '@/components/Hero'
 import LinksSection from '@/components/LinksSection'
+import Link from 'next/link'
 import { useState } from 'react'
 
 export default function Home() {
@@ -12,7 +13,7 @@ export default function Home() {
 		<>
 			<section className="w-full flex flex-col font-light py-6 divide-x divide-slate-200 sm:justify-between uppercase text-slate-500 text-sm sm:text-xl bg-white">
 				<div className="flex justify-center px-4 flex-wrap flex-1 text-center space-x-2">
-					<div className="whitespace-nowrap py-1 px-2 border border-slate-100 rounded">
+					<div className="whitespace-nowrap py-1 px-2 border border-slate-100 bg-slate-50 rounded">
 						1 XNO = 0.67 USD
 					</div>
 				</div>
@@ -22,17 +23,23 @@ export default function Home() {
 					<div className="flex flex-1 flex-col justify-center">
 						<Hero />
 					</div>
-					<div className="flex flex-1 flex-col justify-center">
-						<div className="w-96 sm:w-[448px] max-w-full px-4 py-4 sm:py-8">
+					<div className="flex flex-1 flex-col justify-center gap-8">
+						<div className="w-96 sm:w-[448px] max-w-full px-4 sm:py-8">
 							<AddressInput
 								onValidAddress={setNanoAddress}
 								onInvalidAddress={() => setNanoAddress(null)}
 								onSubmit={console.log}
 							/>
 						</div>
-						<div className="w-full flex justify-center py-4">
+						<div className="w-full flex justify-center">
 							<CheckBox nanoAddress={nanoAddress || undefined} />
 						</div>
+						<p className="text-xs text-slate-500 text-center">
+							By submitting, you accept the{' '}
+							<Link href="/tos" className="text-sky-500">
+								Terms of Service
+							</Link>
+						</p>
 					</div>
 				</div>
 			</section>
