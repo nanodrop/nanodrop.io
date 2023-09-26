cded6d3b753f4519c595603dfddefda0e8aacee0
anarkrypto <anarkrypto@gmail.com>
2023-09-26 18:20:55 -0300

    🥅 Improve fetcher cathing errors

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,10 +1,30 @@
 const fetcher = async (url: string) => {
-	const response = await fetch(url)
-	if (!response.ok) {
-		throw new Error(`status error: ${response.statusText}`)
+	try {
+		const response = await fetch(url)
+		if (!response.ok) {
+			throw response
+		}
+		const data = await response.json()
+		return data
+	} catch (error) {
+		let message = 'Fetch error'
+		if (error instanceof Response) {
+			message = error.statusText
+			try {
+				const data = await error.json()
+				if (typeof data.error === 'string') {
+					message = data.error
+				} else if (typeof data.message === 'string') {
+					message = data.message
+				}
+			} catch {}
+		} else if (error instanceof DOMException) {
+			message = 'Error parsing JSON'
+		} else if (error instanceof Error) {
+			message = error.message
+		}
+		throw message
 	}
-	const data = await response.json()
-	return data
 }
 
 export default fetcher
