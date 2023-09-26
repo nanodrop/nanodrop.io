69a5cb2806afb50deb0fccc46d999a34acd6a33c
anarkrypto <anarkrypto@gmail.com>
2023-09-26 16:45:35 -0300

    Set default value in total drops

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
 			<h4
 				className={`${montserrat.className} font-normal text-sm text-slate-500 mt-4`}
 			>
-				205.343 Drops!{' '}
+				+200K Drops!{' '}
 				<Link
 					href={
 						'https://www.nanolooker.com/account/nano_3dropio1aj6yttxeqf7wm16u3eofx7kkcff5ytcy9m11zxh3uj6r9k5yussb'
