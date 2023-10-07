3ff708f5f8933ef93c37f2186a5120bc082409b4
anarkrypto <anarkrypto@gmail.com>
2023-10-07 01:11:13 -0300

    💄 Move price widget to Appbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,15 @@ import AddressInput from '@/components/AddressInput'
 import CheckBox from '@/components/Checkbox'
 import Hero from '@/components/Hero'
 import LinksSection from '@/components/LinksSection'
-import usePrice from '@/hooks/usePrice'
 import Link from 'next/link'
 import { useState } from 'react'
 
 export default function Home() {
 	const [nanoAddress, setNanoAddress] = useState<string | null>(null)
 
-	const { price, error: priceError, isLoading: priceIsLoading } = usePrice()
-
 	return (
 		<div className="flex flex-col flex-1 justify-center w-full max-w-5xl">
-			<section className="flex flex-col w-full justify-center items-center py-8 bg-white">
-				<div className="whitespace-nowrap py-1 px-2 border border-slate-100 bg-slate-50 rounded text-slate-500 text-sm sm:text-base">
-					1 XNO ={' '}
-					{priceError ? priceError : priceIsLoading ? '-- USD' : `${price} USD`}
-				</div>
-			</section>
-			<section className="flex flex-col w-full flex-1 justify-center items-center py-8 bg-white">
+			<section className="flex flex-col w-full flex-1 justify-center items-center py-8">
 				<div className="w-full flex flex-col items-center justify-between gap-8">
 					<div className="flex flex-col justify-center">
 						<Hero />
