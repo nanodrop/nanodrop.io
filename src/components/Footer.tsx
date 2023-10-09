37e4b448d8abebda83353274b1fc587cd7741624
anarkrypto <anarkrypto@gmail.com>
2023-10-09 16:37:24 -0300

    💄 Improve footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,35 @@
 import { SOCIALS } from '@/config'
-import { EnvelopeIcon } from '@heroicons/react/24/solid'
 import Link from 'next/link'
 
 const navigation = {
-	main: [{ name: 'Terms of Service', href: '/tos' }],
+	main: [
+		{
+			name: 'Wallets',
+			href: 'https://hub.nano.org/wallets/open-source/75/allows-representative-changes/76/non-custodial/291',
+			external: true,
+		},
+		{ name: 'Donate', href: '/donate' },
+		{ name: 'Terms', href: '/tos' },
+	],
 	social: [
+		{
+			name: 'GitHub',
+			href: SOCIALS.github,
+			icon: (props: React.SVGProps<SVGSVGElement>) => (
+				<svg viewBox="0 0 20 20" version="1.1" {...props}>
+					<g stroke="none" strokeWidth="1" fillRule="evenodd">
+						<g
+							transform="translate(-140.000000, -7559.000000)"
+							fill="currentColor"
+						>
+							<g id="icons" transform="translate(56.000000, 160.000000)">
+								<path d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"></path>
+							</g>
+						</g>
+					</g>
+				</svg>
+			),
+		},
 		{
 			name: 'Discord',
 			href: SOCIALS.discord,
@@ -21,24 +46,6 @@ const navigation = {
 							fill="currentColor"
 							fillRule="nonzero"
 						></path>
-					</g>
-				</svg>
-			),
-		},
-		{
-			name: 'GitHub',
-			href: SOCIALS.github,
-			icon: (props: React.SVGProps<SVGSVGElement>) => (
-				<svg viewBox="0 0 20 20" version="1.1" {...props}>
-					<g stroke="none" strokeWidth="1" fillRule="evenodd">
-						<g
-							transform="translate(-140.000000, -7559.000000)"
-							fill="currentColor"
-						>
-							<g id="icons" transform="translate(56.000000, 160.000000)">
-								<path d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"></path>
-							</g>
-						</g>
 					</g>
 				</svg>
 			),
@@ -77,60 +84,62 @@ const navigation = {
 
 export default function Footer() {
 	return (
-		<footer className="w-full bg-slate-50 pb-safe border-t border-slate-100">
-			<div className="mx-auto max-w-7xl overflow-hidden py-2 space-y-4 px-6 sm:py-4 lg:px-8">
-				<div className="flex justify-center space-x-10">
-					{navigation.social.map(item => (
-						<a
-							key={item.name}
-							href={item.href}
-							className="text-slate-400 hover:text-sky-600"
-						>
-							<span className="sr-only">{item.name}</span>
-							<item.icon className="h-6 w-6" aria-hidden="true" />
-						</a>
-					))}
-				</div>
-				<div className="flex space-x-3 justify-center items-center">
-					<nav
-						className="flex justify-center space-x-2 sm:space-x-4"
-						aria-label="Footer"
-					>
-						{navigation.main.map(item => (
-							<div key={item.name}>
-								<a
-									href={item.href}
-									className="text-[12px] leading-6 text-slate-500 hover:text-slate-900"
-								>
-									{item.name}
-								</a>
+		<footer className="border-t border-slate-100 bg-slate-50 pb-safe">
+			<div className="w-full max-w-7xl mx-auto">
+				<div className="flex flex-col items-center justify-center gap-y-12 pt-6 pb-4 lg:flex-row lg:items-center">
+					<div>
+						<div className="flex justify-center items-center text-slate-900">
+							<div className="flex gap-6">
+								{navigation.social.map(item => (
+									<a
+										key={item.name}
+										href={item.href}
+										className="flex items-center gap-1 text-slate-500 hover:text-nano text-sm"
+									>
+										<item.icon className="h-6 w-6" aria-hidden="true" />
+										<span className="sr-only">{item.name}</span>
+									</a>
+								))}
 							</div>
-						))}
-					</nav>
-					<span className="text-slate-400">·</span>
-					<div className="flex justify-center">
-						<a
-							href={'mailto:hello@nanodrop.io'}
-							className="flex items-center space-x-1 text-[12px] leading-6 text-slate-500 hover:text-slate-900"
-						>
-							<EnvelopeIcon className="w-3 h-3" />
-							<span>hello@nanodrop.io</span>
-						</a>
+						</div>
+						<nav className="mt-4 flex gap-8">
+							<ul className="flex items-center gap-3" aria-label="Footer">
+								{navigation.main.map((item, index) => (
+									<>
+										<li key={item.name} className="flex gap-3">
+											<Link
+												href={item.href}
+												className="flex gap-1 items-center text-[12px] text-slate-500 hover:text-nano"
+											>
+												{item.name}
+											</Link>
+											{index < navigation.main.length - 1 && (
+												<span className="text-slate-500">·</span>
+											)}
+										</li>
+									</>
+								))}
+							</ul>
+						</nav>
 					</div>
 				</div>
-				<div className="flex space-x-2 justify-center text-center text-[12px] leading-5 text-slate-500">
-					<span>Ӿ 2023 NanoDrop</span>
-					<span>·</span>
-					<span>
-						Created by{' '}
+				<div className="flex flex-col items-center border-t border-slate-200 py-4 md:flex-row-reverse md:justify-between">
+					<Link
+						href="mailto:hello@nanodrop.io"
+						className="text-xs text-slate-500 hover:text-nano"
+					>
+						hello@nanodrop.io
+					</Link>
+					<p className="mt-6 text-xs text-slate-500 md:mt-0">
+						Ӿ {new Date().getFullYear()} NanoDrop by{' '}
 						<Link
 							target="_blank"
-							href="https://github.com/anarkrypto"
+							href="https:github.com/anarkrypto"
 							className="text-sky-600 hover:underline"
 						>
 							Anarkrypto
 						</Link>
-					</span>
+					</p>
 				</div>
 			</div>
 		</footer>
