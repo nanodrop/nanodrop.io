97896b9d0771932ca434009145eee7d6e04b4469
anarkrypto <anarkrypto@gmail.com>
2023-10-10 09:49:52 -0300

    ♻️ Refactor Checkbox into useFaucet hook

--- src/components/Checkbox/CheckBox.tsx
+++ src/components/Checkbox/CheckBox.tsx
@@ -1,194 +1,65 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect } from 'react'
 import Image from 'next/image'
-import { ButtonBase } from '@mui/material'
-import useSWR from 'swr'
-import useSWRMutation from 'swr/mutation'
-import Turnstile, { BoundTurnstileObject, useTurnstile } from 'react-turnstile'
 
-import { checkHash, convert, Unit } from 'nanocurrency'
+import clsx from 'clsx'
+import { ButtonBase } from '@mui/material'
+import { convert, Unit } from 'nanocurrency'
+
+import useFaucet from '@/hooks/useFaucet'
 import './checkbox.css'
 
 import RefreshSVG from './assets/refresh.svg'
 import XnoDropSvg from './assets/xno-drop.svg'
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL
+import { explorerLinkFromHash } from '@/utils'
 
 export interface CheckBoxProps {
 	nanoAddress?: string
 }
 
-interface Ticket {
-	amount: string
-	ticket: string
-	createdAt: number
-	expiresAt: number
-	verificationRequired: boolean
-}
-
-const rawToNano = (raw: string) => {
-	return convert(raw, {
-		from: Unit.raw,
-		to: Unit.NANO,
-	})
-}
-
-const getTicket = async (): Promise<Ticket> => {
-	const response = await fetch(`${API_URL}/ticket`)
-	if (response.ok) {
-		return await response.json()
-	} else {
-		let message = `Failed with status ${response.status}`
-		try {
-			const body = await response.json()
-			if ('message' in body) {
-				message = body.message
-			} else if ('error' in body && typeof body.error === 'string') {
-				message = body.error
-			}
-		} catch (err) {}
-		throw new Error(message)
-	}
-}
-
-interface DropMeRequest {
-	account: string
-	ticket: string
-	turnstileToken?: string
-}
-
-const dropMe = async ({
-	account,
-	ticket,
-	turnstileToken,
-}: DropMeRequest): Promise<any> => {
-	const response = await fetch(`${API_URL}/drop`, {
-		method: 'POST',
-		body: JSON.stringify({ account, ticket, turnstileToken }),
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	})
-	if (response.ok) {
-		return await response.json()
-	} else {
-		let message = `Failed with status ${response.status}`
-		try {
-			const body = await response.json()
-			if ('message' in body) {
-				message = body.message
-			} else if ('error' in body && typeof body.error === 'string') {
-				message = body.error
-			}
-		} catch (err) {}
-		throw new Error(message)
-	}
-}
-
 export default function CheckBox({ nanoAddress }: CheckBoxProps) {
-	const [errorTitle, setErrorTitle] = useState<string | null>(null)
-	const [errorMessage, setErrorMessage] = useState<string | null>(null)
-	const [isSent, setIsSent] = useState(false)
-	const [isRefreshing, setIsRefreshing] = useState(false)
-	const [turnstileToken, setTurnstileToken] = useState('')
-
-	const turnstile = useTurnstile()
-
-	const { data: ticket, isLoading: isLoadingTicket } = useSWR(
-		'ticket',
-		getTicket,
-		{
-			refreshInterval: 1000 * 60 * 4, // 4 minutes
-			onError: (error: Error) => {
-				setErrorTitle('Ticket Error')
-				setErrorMessage(error.message)
-			},
-		},
-	)
-
 	const {
-		data,
-		trigger: drop,
-		isMutating: isDropping,
-	} = useSWRMutation(
-		{ account: nanoAddress, ticket: ticket?.ticket, turnstileToken },
-		data => dropMe(data as DropMeRequest),
-		{
-			onError: (error: Error) => {
-				setErrorTitle('Error Sending')
-				setErrorMessage(error.message)
-			},
-			onSuccess: data => {
-				if ('hash' in data && checkHash(data.hash)) {
-					setIsSent(true)
-				} else {
-					setErrorTitle('Error Sending')
-					setErrorMessage('Hash not found')
-				}
-			},
+		drop,
+		amount,
+		isDropping,
+		isReady,
+		isVerifying,
+		Verification,
+		isError,
+		error,
+		isSent,
+		dropData,
+		refresh,
+		isRefreshing,
+	} = useFaucet()
+
+	const handleClick = useCallback(
+		async (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+			event.stopPropagation()
+			if (event.isTrusted) {
+				if (isSent) return
+				if (isError) await refresh()
+				await drop(nanoAddress as string)
+			}
 		},
+		[isSent, isError, nanoAddress],
 	)
 
-	const reset = () => {
-		setIsSent(false)
-		setErrorMessage(null)
-		setErrorTitle(null)
-	}
-
-	const handleClick = async (
-		event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-	) => {
-		event.stopPropagation()
-		if (event.isTrusted) {
-			reset()
-			if (!verified) {
-				turnstile?.reset()
-			}
-			await drop()
-		}
-	}
-
-	const [verified, setVerified] = useState(false)
-	const [verifying, setVerifying] = useState(false)
-
 	useEffect(() => {
-		if (turnstile && ticket?.verificationRequired) {
-			setVerifying(true)
-			turnstile.execute()
-		}
-	}, [turnstile, ticket])
-
-	const handleTurstileVerify = (token: string) => {
-		console.log('handleTurstileVerify')
-		setVerifying(false)
-		setVerified(true)
-		setTurnstileToken(token)
-	}
-
-	const handleTurnstileExpired = (turnstile: BoundTurnstileObject) => {
-		setVerified(false)
-		turnstile.reset()
-	}
-
-	const handleTurnstileError = (error: any) => {
-		setErrorTitle('Verification Error')
-		setErrorMessage(JSON.stringify(error))
-	}
-
-	const handleTurnstileUnsupported = () => {
-		setErrorTitle('Verification Error')
-		setErrorMessage('Unsupported')
-	}
-
-	useEffect(() => {
-		if (isSent) {
+		if (dropData && dropData?.account === nanoAddress) {
 			const audio = new Audio('/assets/drip.mp3')
 			audio.play()
 		}
-	}, [isSent])
+	}, [nanoAddress, dropData])
 
-	const isError = !!errorMessage || !!errorTitle
+	useEffect(() => {
+		if (dropData && dropData?.account !== nanoAddress) {
+			refresh()
+		}
+	}, [dropData, nanoAddress])
+
+	const isLoading = !isReady || isVerifying || isDropping
 
 	return (
 		<ButtonBase
@@ -197,26 +68,20 @@ export default function CheckBox({ nanoAddress }: CheckBoxProps) {
 					backgroundColor: '#000',
 				},
 			}}
-			className="!rounded"
-			disabled={verifying}
+			className={clsx('!rounded')}
+			disabled={isLoading}
+			href={isSent ? explorerLinkFromHash(dropData?.hash || '') : ''}
+			target="_blank"
 		>
-			<Turnstile
-				execution="execute"
-				sitekey="0x4AAAAAAAKvYWC-9c5TVGWD"
-				onVerify={handleTurstileVerify}
-				onExpire={(token, turnstile) => handleTurnstileExpired(turnstile)}
-				onError={handleTurnstileError}
-				onTimeout={handleTurnstileExpired}
-				onUnsupported={handleTurnstileUnsupported}
-			/>
+			<Verification />
 			<div
 				id="nd-anchor-container"
-				className="group w-fit max-w-full flex space-x-3 p-3 rounded hover:shadow cursor-pointer h-16 border border-slate-200 bg-[aliceblue] nd-anchor-light"
+				className="group w-fit max-w-full flex space-x-3 p-3 rounded hover:shadow cursor-pointer h-16 border border-slate-200 bg-[aliceblue] nd-anchor-light text-center"
+				onClick={handleClick}
 			>
 				<div
 					id="nd-anchor-content"
 					className="relative inline-flex items-center space-x-2 w-48"
-					onClick={handleClick}
 				>
 					<div id="nd-anchor-checkbox-holder">
 						<div
@@ -226,7 +91,7 @@ export default function CheckBox({ nanoAddress }: CheckBoxProps) {
 							aria-labelledby="recaptcha-anchor-label"
 						>
 							{(isError && <ErrorMark />) ||
-								((isDropping || verifying) && <CheckboxSpinner />) ||
+								(isLoading && <CheckboxSpinner />) ||
 								(isSent && <CheckMark />) || (
 									<div
 										id="recaptcha-checkbox-border"
@@ -247,18 +112,18 @@ export default function CheckBox({ nanoAddress }: CheckBoxProps) {
 							aria-hidden="true"
 							role="presentation"
 						>
-							{((errorMessage || errorTitle) && (
+							{(error && (
 								<>
 									<p aria-label="error-title" className="font-medium text-sm">
-										{errorTitle || 'Error'}
+										{error.title || 'Error'}
 									</p>
 									<p aria-label="error-message" className="text-[10px]">
-										{errorMessage}
+										{error.message}
 									</p>
 								</>
 							)) ||
-								(verifying && <>Safe Verifying</>) ||
-								(isSent && (
+								(isVerifying && <>Safe Verifying</>) ||
+								(isSent && dropData && (
 									<>
 										<span
 											aria-live="polite"
@@ -266,7 +131,10 @@ export default function CheckBox({ nanoAddress }: CheckBoxProps) {
 										></span>
 										Sent{' '}
 										<span id="drop-amount">
-											{ticket ? rawToNano(data.amount) : 'some'}
+											{convert(dropData.amount, {
+												from: Unit.raw,
+												to: Unit.NANO,
+											})}
 										</span>{' '}
 										Nano
 									</>
@@ -278,7 +146,12 @@ export default function CheckBox({ nanoAddress }: CheckBoxProps) {
 										></span>
 										Send{' '}
 										<span id="drop-amount">
-											{ticket ? rawToNano(ticket.amount) : 'some'}
+											{amount !== undefined
+												? convert(amount, {
+														from: Unit.raw,
+														to: Unit.NANO,
+												  })
+												: 'some'}
 										</span>{' '}
 										Nano
 									</>
@@ -305,6 +178,22 @@ export default function CheckBox({ nanoAddress }: CheckBoxProps) {
 							<div className="flex flex-col items-center">
 								<Image src={RefreshSVG} alt={''} className="w-5 h-5" />
 								<div className="nd-anchor-logo-text-refresh">Refresh</div>
+							</div>
+						)) ||
+						(isSent && (
+							<div className="flex flex-col items-center relative group">
+								<svg
+									className="w-[18px] h-[18px] text-slate-500 group-hover:text-nano mx-auto"
+									viewBox="0 0 512 512.0015"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="currentColor"
+								>
+									<path d="m220.769531 376.726562-47.066406 47.066407c-23.570313 23.570312-61.921875 23.578125-85.503906 0-23.566407-23.570313-23.558594-61.921875.011719-85.492188l94.503906-94.503906c21.839844-21.839844 56.378906-23.453125 80.078125-4.816406 1.875 1.472656 3.683593 3.078125 5.414062 4.808593 10.496094 10.492188 26.769531 10.664063 37.046875.382813l29.617188-29.617187c-4.867188-7.386719-10.492188-14.277344-16.824219-20.609376-8.925781-8.921874-18.824219-16.292968-29.371094-22.09375-41.265625-22.761718-92.300781-21.625-132.601562 3.40625-8.371094 5.199219-16.289063 11.429688-23.554688 18.699219l-94.152343 94.152344c-51.152344 51.152344-51.152344 134.378906-.007813 185.527344 51.15625 51.152343 134.378906 51.152343 185.535156 0l84.699219-84.699219c-29.585938 3.914062-60.039062-.148438-87.824219-12.210938zm0 0"></path>
+									<path d="m473.636719 38.359375c-51.148438-51.148437-134.371094-51.148437-185.527344.007813l-84.691406 84.691406c29.585937-3.917969 60.039062.144531 87.824219 12.210937l47.070312-47.070312c23.570312-23.570313 61.914062-23.570313 85.484375 0 23.578125 23.578125 23.578125 61.921875.007813 85.492187l-94.515626 94.515625c-21.597656 21.597657-55.875 23.089844-79.546874 4.542969-1.9375-1.503906-3.789063-3.148438-5.566407-4.925781-10.320312-10.320313-27.105469-10.3125-37.425781.007812l-29.617188 29.617188c4.871094 7.371093 10.503907 14.269531 16.824219 20.589843 8.988281 8.988282 18.972657 16.394532 29.59375 22.226563 41.492188 22.777344 92.785157 21.445313 133.113281-3.980469 8.101563-5.105468 15.769532-11.1875 22.816407-18.238281l94.15625-94.152344c51.152343-51.15625 51.152343-134.378906 0-185.535156zm0 0"></path>
+								</svg>
+								<div className="text-xs mt-1 text-slate-500 group-hover:text-nano">
+									Explorer
+								</div>
 							</div>
 						)) || (
 							<div
