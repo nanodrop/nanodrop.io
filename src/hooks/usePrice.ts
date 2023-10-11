10bd4e3da58e590d2cc446793ecd3ef5df50e375
anarkrypto <anarkrypto@gmail.com>
2023-10-11 18:16:47 -0300

    🩹 Fix price format

--- src/hooks/usePrice.ts
+++ src/hooks/usePrice.ts
@@ -2,13 +2,17 @@ import useSWR from 'swr'
 import fetcher from '@/lib/fetcher'
 import { LatestPrice } from '@/services/coinmarketcap'
 
-const limit2Decimals = (value: number) => Math.floor(value * 100) / 100
+const formatPrice = (value: number): string => {
+	const parts = value.toString().split('.')
+	if (parts.length === 1) return parts[0]
+	return `${parts[0]}.${parts[1].substring(0, 2).padEnd(2, '0')}`
+}
 
 export default function usePrice() {
 	const { data, error, isLoading } = useSWR<LatestPrice>('/api/price', fetcher)
 
 	return {
-		price: data ? limit2Decimals(data.price) : null,
+		price: data ? formatPrice(data.price) : null,
 		percent_change_24h: data?.percent_change_24h ?? null,
 		error: (error as string) || null,
 		isLoading,
