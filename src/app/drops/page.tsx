12e7930a77870535cc598f5d219130c9d650bccd
anarkrypto <anarkrypto@gmail.com>
2023-10-15 05:23:39 -0300

    ✨ Add DropsMap

--- src/app/drops/page.tsx
+++ src/app/drops/page.tsx
@@ -4,10 +4,15 @@ import useListDrops from '@/hooks/useListDrops'
 import { ArrowPathIcon } from '@heroicons/react/24/solid'
 import { ButtonBase, IconButton, Skeleton } from '@mui/material'
 import clsx from 'clsx'
+
 import DropListTableDesktop from './_components/DropListTableDesktop'
 import DropListTableMobile from './_components/DropListTableMobile'
+import DropsMap from '@/components/DropsMap'
+import { usePreferences } from '@/contexts/PreferencesProvider'
 
 export default function Drops() {
+	const { darkMode } = usePreferences()
+
 	const { drops, isLoading, error, refresh, isRefreshing, hasMore, loadMore } =
 		useListDrops()
 	if (error)
@@ -29,47 +34,59 @@ export default function Drops() {
 		)
 	}
 	return (
-		<div className="px-2 sm:px-6 lg:px-8">
-			<div className="flex">
-				<div className="flex-auto">
-					<h1 className="text-base font-semibold leading-6 text-slate-900 dark:text-zinc-400">
-						Drop List
+		<div className="flex flex-col flex-1 2xl:flex-row 2xl:justify-between 2xl:gap-4 overflow-hidden">
+			<div className="w-full relative flex flex-col flex-1 2xl:w-3/5 max-w-7xl max-h-[60%] 2xl:max-h-max">
+				<DropsMap theme={darkMode ? 'dark' : 'light'} />
+				<div className="w-full absolute top-0 left-0 p-2">
+					<h1 className="text-sm font-semibold text-slate-400 dark:text-zinc-600">
+						203.443 Drops
 					</h1>
-					<p className="mt-2 text-xs sm:text-sm">
-						A list of all transactions sent by NanoDrop
-					</p>
-				</div>
-				<div className="sm:mt-6 sm:ml-16 sm:flex-none">
-					<IconButton
-						type="button"
-						onClick={refresh}
-						disabled={isLoading || isRefreshing}
-						className="group"
-					>
-						<ArrowPathIcon
-							className={clsx(
-								'w-6 sm:w-8 h-6 sm:h-8 text-slate-600 dark:text-zinc-600 group-hover:text-nano',
-								isRefreshing && 'animate animate-spin',
-							)}
-						/>
-					</IconButton>
 				</div>
 			</div>
-			<div className="mt-4 sm:mt-8 hidden sm:flow-root">
-				<DropListTableDesktop drops={drops || []} />
-			</div>
-			<div className="mt-4 sm:mt-8 flow-root sm:hidden">
-				<DropListTableMobile drops={drops || []} />
-			</div>
-			{hasMore && (
-				<div className="flex justify-center p-4">
-					<ButtonBase onClick={loadMore}>
-						<div className="bg-nano px-4 py-2 font-semibold text-white uppercase rounded-lg">
-							Load More
+			<div className="flex flex-col flex-1 px-4 2xl:py-4 overflow-hidden">
+				<div className="flex">
+					<div className="flex-auto">
+						<h1 className="text-base font-semibold leading-6 text-slate-900 dark:text-zinc-400">
+							Drop List
+						</h1>
+						<p className="mt-2 text-xs sm:text-sm">
+							A list of all transactions sent by NanoDrop
+						</p>
+					</div>
+					<div className="sm:mt-6 sm:ml-16 sm:flex-none">
+						<IconButton
+							type="button"
+							onClick={refresh}
+							disabled={isLoading || isRefreshing}
+							className="group"
+						>
+							<ArrowPathIcon
+								className={clsx(
+									'w-6 sm:w-8 h-6 sm:h-8 text-slate-600 dark:text-zinc-600 group-hover:text-nano',
+									isRefreshing && 'animate animate-spin',
+								)}
+							/>
+						</IconButton>
+					</div>
+				</div>
+				<div className="flex flex-col flex-1 overflow-y-auto">
+					<div className="mt-4 sm:mt-8 hidden sm:flow-root">
+						<DropListTableDesktop drops={drops || []} />
+					</div>
+					<div className="mt-4 sm:mt-8 flow-root sm:hidden">
+						<DropListTableMobile drops={drops || []} />
+					</div>
+					{hasMore && (
+						<div className="flex justify-center p-4">
+							<ButtonBase onClick={loadMore}>
+								<div className="bg-nano px-4 py-2 font-semibold text-white uppercase rounded-lg">
+									Load More
+								</div>
+							</ButtonBase>
 						</div>
-					</ButtonBase>
+					)}
 				</div>
-			)}
+			</div>
 		</div>
 	)
 }
---
+++ src/components/DropsMap.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useRef, useState } from 'react'
+import WorldMap, { DataItem } from 'react-svg-worldmap'
+import { slate } from 'tailwindcss/colors'
+
+const data: DataItem[] = []
+
+interface DropsMapProps {
+	theme: 'light' | 'dark'
+}
+
+export default function DropsMap({ theme }: DropsMapProps) {
+	const [width, setWidth] = useState(0)
+
+	const containerRef = useRef<HTMLDivElement>(null)
+
+	const loadWidth = () => {
+		if (containerRef.current) {
+			const rect = containerRef.current.getBoundingClientRect()
+
+			// ensure 3/4 aspect ratio
+			setWidth(
+				rect.width > (rect.height * 4) / 3 ? (rect.height * 4) / 3 : rect.width,
+			)
+		}
+	}
+
+	useEffect(loadWidth, [containerRef])
+
+	useEffect(() => {
+		window.addEventListener('resize', loadWidth)
+		return () => window.removeEventListener('resize', loadWidth)
+	}, [])
+
+	return (
+		<div
+			className="w-full flex flex-col flex-1 items-center justify-center overflow-hidden"
+			ref={containerRef}
+		>
+			<WorldMap
+				borderColor={theme === 'dark' ? slate[600] : slate[400]}
+				color="#209CE9"
+				backgroundColor="transparent"
+				value-suffix="people"
+				size={width || 'responsive'}
+				data={data}
+				richInteraction
+			/>
+		</div>
+	)
+}
