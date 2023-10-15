6dd1aba7e4ad5430f1deaafc5351805a1e93699f
anarkrypto <anarkrypto@gmail.com>
2023-10-15 05:23:01 -0300

    💄 Drops layout: Add max-height and update max-width

--- src/app/drops/layout.tsx
+++ src/app/drops/layout.tsx
@@ -1,5 +1,14 @@
 import { ReactNode } from 'react'
 
 export default function DropsLayout({ children }: { children: ReactNode }) {
-	return <div className="w-full max-w-7xl p-4">{children}</div>
+	return (
+		<div
+			className="w-full max-w-6xl 2xl:max-w-none overflow-y-hidden flex flex-col"
+			style={{
+				height: 'calc(100vh - 60px)',
+			}}
+		>
+			{children}
+		</div>
+	)
 }
