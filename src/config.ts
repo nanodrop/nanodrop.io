e4df1f9e0cc03084a49e37e9813dbb7b5fb1858c
anarkrypto <anarkrypto@gmail.com>
2025-08-11 02:32:05 -0300

    update discord and reddit links to official channels

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ export const DARK_MODE_BY_DEFAULT = false
 
 export const SOCIALS = {
 	github: 'https://github.com/nanodrop/nanodrop.io',
-	discord: 'https://discord.gg/NQ8QnGa3dH',
-	reddit: 'https://www.reddit.com/user/anarkrypto',
+	discord: 'https://chat.nano.org/',
+	reddit: 'https://www.reddit.com/r/nanocurrency/',
 }
 
 export const CONTACT_EMAIL = 'hello@nanodrop.io'
